6fa31c92977e2f2b1672705612fbd56b9060ad27
pavansudeesh <pavanperuru173@gmail.com>
2022-05-31 15:30:18 +0530

    changed the deployment file and gave sudo permissions

--- deployment.sh
+++ deployment.sh
@@ -1,2 +1,2 @@
-DOCKER_BUILDKIT=1 docker build . --build-arg ROS_DISTRO=melodic --build-arg LOCAL_WS_DIR=./robot_ws --build-arg APP_NAME=helloworld-robot-app -t robomaker-helloworld-robot-app
-DOCKER_BUILDKIT=1 docker build . --build-arg GAZEBO_VERSION=gazebo-9 --build-arg ROS_DISTRO=melodic --build-arg LOCAL_WS_DIR=./simulation_ws --build-arg APP_NAME=helloworld-sim-app -t robomaker-helloworld-sim-app
+DOCKER_BUILDKIT=1 sudo docker build . --build-arg ROS_DISTRO=melodic --build-arg LOCAL_WS_DIR=./robot_ws --build-arg APP_NAME=helloworld-robot-app -t robomaker-helloworld-robot-app
+DOCKER_BUILDKIT=1 sudo docker build . --build-arg GAZEBO_VERSION=gazebo-9 --build-arg ROS_DISTRO=melodic --build-arg LOCAL_WS_DIR=./simulation_ws --build-arg APP_NAME=helloworld-sim-app -t robomaker-helloworld-sim-app
